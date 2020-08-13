20d34f773d9c0bcd60271e7486bd71e3daa0a7b1
kaznak <kaznak.at.work@gmail.com>
2020-08-13 21:54:09 +0900

    server.js: add a handler, on connection

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
 
 const port = 3000;
